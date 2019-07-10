69ca2065df9ddd936bfc1a45d7e3bc706e06c40c
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-10 16:12:20 -0700

    enemy AI to control army
    reinforce when time runs out

--- Assets/Scripts/EnemyArmyController.cs
+++ Assets/Scripts/EnemyArmyController.cs
@@ -6,19 +6,52 @@ namespace AncientArmory
 {
     public class EnemyArmyController : MonoBehaviour
     {
+        private const int reinforcementsPollsSecondsDelay = 1;
+
         [SerializeField]
         private ArmyController armyController;
+
+        /// <summary>
+        /// How many soldiers does the Enemy want to have on their side? Call reinforcements to fill to this amount.
+        /// </summary>
+        [SerializeField]
+        private int desiredCombatantsOnField = 3;
+        
+        private float nextReinforcementsTime;
+
+        private Coroutine coroutine_reinforcements;
 
         // Start is called before the first frame update
         void Start()
         {
-
+            coroutine_reinforcements = StartCoroutine(HandleReinforcements());
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        private IEnumerator HandleReinforcements()
+        {
+            while (true)
+            {
+                if(Time.time > nextReinforcementsTime)
+                {
+                    CallReinforcements();
+                }
+
+                yield return new WaitForSeconds(reinforcementsPollsSecondsDelay);
+            }
+        }
+
+        private void CallReinforcements()
+        {
+            if (armyController.GetAliveCombatants() < desiredCombatantsOnField)//check for available space
+            {
+                //spawn more enemies!
+            }
         }
     }
 }
