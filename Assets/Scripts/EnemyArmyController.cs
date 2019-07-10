09668c600cc4da20cb4ca4a1954de21e39d5a7f5
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-10 15:10:09 -0700

    new script for controlling enemyArmy

---
+++ Assets/Scripts/EnemyArmyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AncientArmory
+{
+    public class EnemyArmyController : MonoBehaviour
+    {
+        [SerializeField]
+        private ArmyController armyController;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
