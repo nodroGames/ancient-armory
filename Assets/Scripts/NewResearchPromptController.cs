c2a1cd00bfa5ec6ffcef85cfd5afb940609f701e
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-10 01:33:20 -0700

    new controller script
    script added to prefab and references filled
    deleted extra background image
    
    used UnityEvents and linked button to functions to implement them

---
+++ Assets/Scripts/NewResearchPromptController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using TMPro;
+
+public class NewResearchPromptController : MonoBehaviour
+{
+    [Header("---UI Elements---")]
+    [SerializeField]
+    private TextMeshProUGUI researchInfoText;
+
+    [SerializeField]
+    private TextMeshProUGUI windowTitleText;
+
+    [SerializeField]
+    private Image backgroundImage;
+
+    //events
+    public UnityEvent acceptResearch;
+    public UnityEvent rejectResearch;
+
+    private void Start()
+    {
+        InitEvents();
+    }
+
+    private void InitEvents()
+    {
+        if(acceptResearch == null)
+        {
+            acceptResearch = new UnityEvent();
+        }
+
+        if (rejectResearch == null)
+        {
+            rejectResearch = new UnityEvent();
+        }
+    }
+
+    /// <summary>
+    /// Called by Button.
+    /// </summary>
+    public void AcceptResearch()
+    {
+        acceptResearch.Invoke();
+    }
+
+    /// <summary>
+    /// Called by Button.
+    /// </summary>
+    public void RejectResearch()
+    {
+        rejectResearch.Invoke();
+    }
+}
