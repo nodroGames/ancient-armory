43fa78ac4a90371a6bd7b57c5c2ad3b17a8ed807
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2019-07-09 18:44:43 -0700

    new prefab and controller

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerController : MonoBehaviour
+{
+    [SerializeField]
+    private TimerMode timerMode;
+
+    [Header("---UI Elements---")]
+    [SerializeField]
+    private TextMeshProUGUI timerText;
+
+    [SerializeField]
+    private Slider timerSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartTimer(float givenTime, TimerMode timerMode = TimerMode.Countdown)
+    {
+        this.timerMode = timerMode;
+        switch (timerMode)
+        {
+            case TimerMode.Countdown:
+                break;
+            case TimerMode.Alarm:
+                break;
+        }
+    }
+
+    public void AddTime()
+    {
+
+    }
+
+    public void SubtractTime(float reduceTime)
+    {
+
+    }
+}
